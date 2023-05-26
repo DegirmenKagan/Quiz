527fbf713bd743db81de648437eca77fffa57e54
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-27 02:29:45 +0300

    Model > Question struct created

--- Quiz/Model/Question.swift
+++ Quiz/Model/Question.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 struct Question{
+    let question: String
+    let answers: [String]
+    let correctAnswer: String
     
+    init(q: String, a: [String], correctAnswer:String){
+        self.question = q
+        self.answers = a
+        self.correctAnswer = correctAnswer
+    }
 }
