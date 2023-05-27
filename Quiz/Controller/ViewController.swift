0119907d6aef467f70db17fbef2e1b8ba8b5408a
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-27 11:40:44 +0300

    Controller > buttons and label connected, planned the functions

--- Quiz/Controller/ViewController.swift
+++ Quiz/Controller/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var choiceButton1: UIButton!
+    @IBOutlet weak var choiceButton2: UIButton!
+    @IBOutlet weak var choiceButton3: UIButton!
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Bring first question
+        updateUI()
     }
 
+    func updateUI(){
+        // question choiceButton renewal
+    }
 
+    @IBAction func choiceSelected(_ sender: UIButton) {
+        //check answer
+        
+        //next question
+    }
 }
 
